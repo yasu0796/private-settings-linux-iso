359072fb462185f8b25aa5a5d76a76e9b2f328e9
yasu0796 <yasu0796@msn.com>
2024-11-23 22:21:38 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ISO BUILDER
+
+## How to build
+
+```bash
+git clone
+cd 
+./run.sh
+```
+
+## How to use
+
+### Ubuntu
+
+1. Mount this iso on second Optical media.
+2. Edit grub linux line to `autoinstall` or press `yes` in install process.
+
+## Limitation
+
+Debian preseed only support via same install media or http.
---
+++ run.sh
@@ -0,0 +1,23 @@
+
+echo
+echo ubuntu-cli
+cd ubuntu-cli
+genisoimage -output ../out/ubuntu-cli-seed.iso -volid cidata -joliet -rock user-data meta-data
+cd ..
+
+echo
+echo ubuntu-desktop
+cd ubuntu-desktop
+genisoimage -output ../out/ubuntu-desktop-seed.iso -volid cidata -joliet -rock user-data meta-data
+cd ..
+
+echo
+echo ubuntu-desktop-2004
+cd ubuntu-desktop-2004
+genisoimage -output ../out/ubuntu-desktop-2004-seed.iso -volid cidata -joliet -rock user-data meta-data
+cd ..
+
+
+
+
+
